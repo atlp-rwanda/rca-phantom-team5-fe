230f2ecbef4efa8ceb2367a43b9c123621f0feb2
Elissa Twizeyimana <twizelissa@gmail.com>
2023-04-22 23:22:19 +0200

    ft: sidebar and logout button

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import HomeScreen from 'screens/HomeScreen';
 import NotFoundScreen from 'screens/NotFoundScreen';
 import SignInScreen from 'screens/SignInScreen';
 import SignUpScreen from 'screens/SignUpScreen';
-import Dashboard from 'screens/Dashboaord';
+import Sidebar from 'layouts/Sidebar';
 
 function App() {
   return (
@@ -14,8 +14,8 @@ function App() {
           <Route path='/' element={<HomeScreen />} />
           <Route path='/sign-up' element={<SignUpScreen />} />
           <Route path='/login' element={<SignInScreen />} />
-          <Route path='/dashboard' element={<Dashboard />} />
           <Route path='*' element={<NotFoundScreen />} />
+          <Route path='/dashbord' element={<Sidebar/>} />
         </Routes>
       </Layout>
     </Router>
