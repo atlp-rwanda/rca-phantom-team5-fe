c3196e3cc9853d1ab3fc67c595d07d588496424a
Elissa Twizeyimana <twizelissa@gmail.com>
2023-04-25 15:25:02 +0200

    fixed logout

--- src/redux/api/authApi.tsx
+++ src/redux/api/authApi.tsx
@@ -4,13 +4,10 @@ import axios from 'axios';
 import baseUrl from 'utils/url';
 import { ThunkAction, ThunkDispatch } from 'redux-thunk';
 import { RootState } from '../store';
-import authReducer, { logoutSuccess, logoutFailure } from '../slice/authSlice';
-
-
 
 export const login = createAsyncThunk(
   'auth/login',
-  async (payload: { email: string; password: string, device_id: string }, thunkAPI) => {
+  async (payload: { email: string; password: string; device_id: string }, thunkAPI) => {
     try {
       const { data } = await axios.post(`${baseUrl}/auth/signin`, payload);
       console.log(data);
@@ -18,22 +15,14 @@ export const login = createAsyncThunk(
     } catch (error: any) {
       return thunkAPI.rejectWithValue(error.response.data);
     }
-  }
+  },
 );
 
-export const logout = createAsyncThunk('auth/logout', async (thunkAPI) => {
+export const logout = createAsyncThunk('auth/logout', async () => {
   try {
     const token = localStorage.getItem('userToken');
     const data = await axios.delete(`${baseUrl}/auth/logout`, { headers: { Authorization: `Bearer ${token}` } });
-      thunkAPI.dispatch(logoutSuccess());
   } catch (error: any) {
     console.error(error);
-    return thunkAPI.rejectWithValue(error.response.data);
   }
 });
- 
-
-
-
-
-
--- src/redux/slice/authSlice.tsx
+++ src/redux/slice/authSlice.tsx
@@ -5,10 +5,10 @@ import baseUrl from 'utils/url';
 import { ThunkAction, ThunkDispatch } from 'redux-thunk';
 import { RootState } from '../store';
 
+import { login, logout } from '../api/authApi';
+
 // initialize userToken from local storage
-const userToken = localStorage.getItem('userToken')
-  ? localStorage.getItem('userToken')
-  : null;
+const userToken = localStorage.getItem('userToken') ? localStorage.getItem('userToken') : null;
 
 const initialState = {
   userInfo: null,
@@ -17,46 +17,10 @@ const initialState = {
   success: false,
 };
 
-export const login = createAsyncThunk(
-  'auth/login',
-  async (payload: { email: string; password: string, device_id: string }, thunkAPI) => {
-    try {
-      const { data } = await axios.post(`${baseUrl}/auth/signin`, payload);
-      console.log(data);
-      return data;
-    } catch (error: any) {
-      return thunkAPI.rejectWithValue(error.response.data);
-    }
-  }
-);
-
-export const logout = createAsyncThunk(
-  'auth/logout',
-  async(dispatch: ThunkDispatch<RootState, unknown, AnyAction>) => {
-    try {
-      const token = localStorage.getItem('userToken');
-      await axios.delete(`${baseUrl}/auth/logout`,{headers: {Authorization: `Bearer ${token}`}});
-      localStorage.removeItem('userToken');
-      dispatch(logoutSuccess());
-    } catch (error: any) {
-      console.error(error);
-      dispatch(logoutFailure(error.message));
-    }
-  });
-
-
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  reducers: {
-    logoutSuccess: (state) => {
-      state.userInfo = null;
-      state.userToken = null;
-    },
-    logoutFailure: (state, action) => {
-      console.error(action.payload);
-    },
-  },
+  reducers: {},
   extraReducers: (builder) => {
     builder.addCase(login.pending, (state) => {
       state.loading = true;
@@ -71,9 +35,15 @@ const authSlice = createSlice({
     builder.addCase(login.rejected, (state, action) => {
       state.loading = false;
     });
+    builder.addCase(logout.fulfilled, (state, action) => {
+      state.userInfo = null;
+      state.userToken = null;
+    });
+    builder.addCase(logout.rejected, (state, action) => {
+      console.log('loged out');
+      state.loading = false;
+    });
   },
 });
-
-export const { logoutSuccess, logoutFailure } = authSlice.actions;
 
 export default authSlice.reducer;
