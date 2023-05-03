0adef0a36fe7023795e0d9369aa91579935ab5d6
Twizeyimana Elissa <77528473+twizelissa@users.noreply.github.com>
2023-05-03 11:10:26 +0200

    Ft protected routes (#16)
    * fixing merge errors
    
    * removing jsx wanning

--- src/redux/slice/authSlice.tsx
+++ src/redux/slice/authSlice.tsx
@@ -1,26 +1,35 @@
+// authSlice.tsx
+
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { AnyAction, Dispatch } from 'redux';
 import axios from 'axios';
 import baseUrl from 'utils/url';
 import { ThunkAction, ThunkDispatch } from 'redux-thunk';
-import { RootState } from '../store';
+// import { RootState } from '../store';
 
 import { login, logout } from '../api/authApi';
-
-// initialize userToken from local storage
 const userToken = localStorage.getItem('userToken') ? localStorage.getItem('userToken') : null;
 
 const initialState = {
   userInfo: null,
   userToken,
+  isAuthonticated: false,
   loading: false,
   success: false,
+  state: {
+    isFetching: false,
+  },
+
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  reducers: {},
+  reducers: {
+    setIsFetching: (state) => {
+      state.state.isFetching = true;
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(login.pending, (state) => {
       state.loading = true;
@@ -30,7 +39,9 @@ const authSlice = createSlice({
       state.userInfo = action.payload.data.user_id;
       state.userToken = action.payload.data.access_token;
       state.success = true;
+      state.isAuthonticated = true;
       localStorage.setItem('userToken', action.payload.data.access_token);
+      return state;
     });
     builder.addCase(login.rejected, (state, action) => {
       state.loading = false;
@@ -46,4 +57,5 @@ const authSlice = createSlice({
   },
 });
 
+export const { setIsFetching } = authSlice.actions;
 export default authSlice.reducer;
