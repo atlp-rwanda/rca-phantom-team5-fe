4c23bd6b224f2648586cf7c30fc3a4aa09ec7273
tuyishime jeandamour <tuyishimejeand88@gmail.com>
2023-05-05 02:12:15 +0200

    Update url.ts
    changing the default base url

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,3 +1,3 @@
-// const baseUrl = 'https://phatom-team-5.herokuapp.com/api';
-const baseUrl = 'http://localhost:3003/api';
+const baseUrl = 'https://phatom-team-5.herokuapp.com/api';
+// const baseUrl = 'http://localhost:3003/api';
 export default baseUrl;
