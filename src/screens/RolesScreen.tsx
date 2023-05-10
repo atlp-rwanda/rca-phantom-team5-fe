45fa4a18a25caa2d50efb161260443dc8364bafc
Elissa Twizeyimana <twizelissa@gmail.com>
2023-05-10 11:34:21 +0200

    update roles by user list

--- src/screens/RolesScreen.tsx
+++ src/screens/RolesScreen.tsx
@@ -12,7 +12,6 @@ import { getAllBuses } from '../redux/api/viewBusesApi';
 import { getAllUsers } from '../redux/api/usersApi';
 import { getRoutes } from '../redux/api/routeApi';
 import { UserDetails } from 'utils/types';
-
 import Sidebar from 'layouts/Sidebar';
 import {
   JSXElementConstructor,
@@ -24,6 +23,7 @@ import {
   useEffect,
   useState,
 } from 'react';
+import UpdateRole from './UpdateRoleScreen';
 
 export default function OverviewScreen() {
   const labels = ['Jan', 'Feb', 'Mar', 'Apri', 'May', 'June', 'July', 'Ago', 'Sept', 'Oct', 'Nov', 'Dec'];
@@ -76,7 +76,7 @@ export default function OverviewScreen() {
 
   return (
     <Sidebar>
-      <section className='px-10 pb-10 pt-5'>
+      <section className='px-10 pb-10'>
         <div className='w-full p-5 shadow-2xl shadow-gray-500/50 md:w-2/5'>
           <h1 className='text-4xl font-bold text-primary'>
             Hello <span style={{ fontWeight: 'bold' }}>{user.fname}</span>
@@ -84,239 +84,195 @@ export default function OverviewScreen() {
           <p className='b-4 mt-4 text-lg font-medium text-black'>Welcome to {user.role} Dashboard</p>
         </div>
 
-        <div>
-          <h2 className=' ml-8 mt-10 text-2xl text-lg font-bold'>Role and Permission</h2>
-        </div>
+        <div className='mt-5'>
+          <h2 className='mb-5'>All users in system </h2>
 
-        <div className='mt-10 '>
           <table className='text-left text-sm text-gray-500 dark:text-gray-400'>
-            <thead className='h-full  bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400'>
+            <thead className='h-full w-full bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400'>
               <tr>
                 <th scope='col' className='p-4'>
                   <div className='flex items-center'>
                     <input
-                      id='checkbox-table-1'
+                      id='checkbox-all'
                       type='checkbox'
                       className='h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-800'
                     />
+                    <label htmlFor='checkbox-all' className='sr-only'>
+                      checkbox
+                    </label>
                   </div>
+                </th>
+                <th scope='col' className='px-4 py-3'>
+                  First Name
+                </th>
+                <th scope='col' className='px-4 py-3'>
+                  Last Name
+                </th>
+                <th scope='col' className='px-4 py-3'>
+                  Nid
+                </th>
+
+                <th scope='col' className='px-4 py-3'>
+                  Email
                 </th>
                 <th scope='col' className='px-4 py-3'>
                   Role
                 </th>
                 <th scope='col' className='px-4 py-3'>
-                  Description
-                </th>
-                <th scope='col' className='px-8 py-3'>
                   Action
                 </th>
               </tr>
             </thead>
             <tbody>
-              <tr className='border-b bg-white hover:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-600'>
-                <td className='w-4 p-4'>
-                  <div className='flex items-center'>
-                    <input
-                      id='checkbox-table-1'
-                      type='checkbox'
-                      className='h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-800'
-                    />
-                    <label htmlFor='checkbox-table-1' className='sr-only'>
-                      checkbox
-                    </label>
-                  </div>
-                </td>
-                <td className='p-4'>Operator</td>
-                <td className='p-4'>Manages transportation system</td>
-                <td>
-                  <Link to='#' className='h-[100px] w-10 text-blue-600'>
-                    <Edit />
-                  </Link>
-                  <Link to='#' className='text-green-600'>
-                    <Visibility />
-                  </Link>
-                  <Link to='#' className='text-red-600'>
-                    <Delete />
-                  </Link>
-                </td>
-              </tr>
-              <tr className='border-b bg-white hover:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-600'>
-                <td className='w-4 p-4'>
-                  <div className='flex items-center'>
-                    <input
-                      id='checkbox-table-2'
-                      type='checkbox'
-                      className='h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-800'
-                    />
-                    <label htmlFor='checkbox-table-2' className='sr-only'>
-                      checkbox
-                    </label>
-                  </div>
-                </td>
-                <td className='p-4'>Driver</td>
-                <td className='p-4'> view and update driving data</td>
-                <td>
-                  <Link to='#' className='text-blue-600'>
-                    <Edit />
-                  </Link>{' '}
-                  <Link to='#' className='text-green-600'>
-                    <Visibility />
-                  </Link>
-                  <Link to='#' className='text-red-600'>
-                    <Delete />
-                  </Link>
-                </td>
-              </tr>
-              <tr className='border-b bg-white hover:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-600'>
-                <td className='w-4 p-4'>
-                  <div className='flex items-center'>
-                    <input
-                      id='checkbox-table-3'
-                      type='checkbox'
-                      className='h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-800'
-                    />
-                    <label htmlFor='checkbox-table-3' className='sr-only'>
-                      checkbox
-                    </label>
-                  </div>
-                </td>
-                <td className='p-4'>User </td>
-                <td className='p-4'> Limited access for basic tasks only</td>
-                <td>
-                  <Link to='#' className='text-blue-600'>
-                    <Edit />
-                  </Link>{' '}
-                  <Link to='#' className='text-green-600'>
-                    <Visibility />
-                  </Link>
-                  <Link to='#' className='text-red-600'>
-                    <Delete />
-                  </Link>
-                </td>
-              </tr>
+              {users.map(
+                (user: {
+                  nid: ReactNode;
+                  id: React.Key | null | undefined;
+                  fname:
+                    | string
+                    | number
+                    | boolean
+                    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+                    | React.ReactFragment
+                    | React.ReactPortal
+                    | null
+                    | undefined;
+                  lname:
+                    | string
+                    | number
+                    | boolean
+                    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+                    | React.ReactFragment
+                    | React.ReactPortal
+                    | null
+                    | undefined;
+                  email:
+                    | string
+                    | number
+                    | boolean
+                    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+                    | React.ReactFragment
+                    | React.ReactPortal
+                    | null
+                    | undefined;
+                  role:
+                    | string
+                    | number
+                    | boolean
+                    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+                    | React.ReactFragment
+                    | React.ReactPortal
+                    | null
+                    | undefined;
+                }) => (
+                  <tr
+                    key={user.id}
+                    className='border-b bg-white hover:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-600'
+                  >
+                    <td className='w-4 p-4'>
+                      <div className='flex items-center'>
+                        <input
+                          id={`checkbox-table-${user.id}`}
+                          type='checkbox'
+                          className='h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-800'
+                        />
+                        <label htmlFor={`checkbox-table-${user.id}`} className='sr-only'>
+                          checkbox
+                        </label>
+                      </div>
+                    </td>
+                    <td className='p-4'>{user.fname}</td>
+                    <td className='p-4'>{user.lname}</td>
+                    <td className='p-4'>{user.nid}</td>
+                    <td className='p-4'>{user.email}</td>
+                    <td className='p-4'>{user.role}</td>
+
+                    <td>
+                      <Link to={''} className='h-[100px] w-10 text-blue-600'>
+                        <Edit />
+                      </Link>
+                      {''}
+
+                      <Link to={`#delete-${user.id}`} className='text-red-600'>
+                        <Delete />
+                      </Link>
+                    </td>
+                  </tr>
+                ),
+              )}
             </tbody>
-          </table>
-
-          <div className='mt-10'>
-            <h2 className='mb-5'>All users in system </h2>
-
-            <table className='text-left text-sm text-gray-500 dark:text-gray-400'>
-              <thead className='h-full w-full bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400'>
-                <tr>
-                  <th scope='col' className='p-4'>
-                    <div className='flex items-center'>
-                      <input
-                        id='checkbox-all'
-                        type='checkbox'
-                        className='h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-800'
-                      />
-                      <label htmlFor='checkbox-all' className='sr-only'>
-                        checkbox
-                      </label>
-                    </div>
-                  </th>
-                  <th scope='col' className='px-4 py-3'>
-                    First Name
-                  </th>
-                  <th scope='col' className='px-4 py-3'>
-                    Last Name
-                  </th>
-                  <th scope='col' className='px-4 py-3'>
-                    Nid
-                  </th>
-
-                  <th scope='col' className='px-4 py-3'>
-                    Email
-                  </th>
-                  <th scope='col' className='px-4 py-3'>
-                    Role
-                  </th>
-                  <th scope='col' className='px-4 py-3'>
-                    Action
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.map(
-                  (user: {
-                    nid: ReactNode;
-                    id: React.Key | null | undefined;
-                    fname:
-                      | string
-                      | number
-                      | boolean
-                      | React.ReactElement<any, string | React.JSXElementConstructor<any>>
-                      | React.ReactFragment
-                      | React.ReactPortal
-                      | null
-                      | undefined;
-                    lname:
-                      | string
-                      | number
-                      | boolean
-                      | React.ReactElement<any, string | React.JSXElementConstructor<any>>
-                      | React.ReactFragment
-                      | React.ReactPortal
-                      | null
-                      | undefined;
-                    email:
-                      | string
-                      | number
-                      | boolean
-                      | React.ReactElement<any, string | React.JSXElementConstructor<any>>
-                      | React.ReactFragment
-                      | React.ReactPortal
-                      | null
-                      | undefined;
-                    role:
-                      | string
-                      | number
-                      | boolean
-                      | React.ReactElement<any, string | React.JSXElementConstructor<any>>
-                      | React.ReactFragment
-                      | React.ReactPortal
-                      | null
-                      | undefined;
-                  }) => (
-                    <tr
-                      key={user.id}
-                      className='border-b bg-white hover:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-600'
+            <nav className='flex items-center justify-center pt-4' aria-label='Table navigation'>
+              <ul className='mb-4 inline-flex items-center gap-4'>
+                <li>
+                  <a
+                    href='#'
+                    className='ml-0 block rounded-l-lg bg-white px-3 py-2 leading-tight  text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                  >
+                    <span className='sr-only'>Previous</span>
+                    <svg
+                      className='h-5 w-5'
+                      aria-hidden='true'
+                      fill='currentColor'
+                      viewBox='0 0 20 20'
+                      xmlns='http://www.w3.org/2000/svg'
                     >
-                      <td className='w-4 p-4'>
-                        <div className='flex items-center'>
-                          <input
-                            id={`checkbox-table-${user.id}`}
-                            type='checkbox'
-                            className='h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-800'
-                          />
-                          <label htmlFor={`checkbox-table-${user.id}`} className='sr-only'>
-                            checkbox
-                          </label>
-                        </div>
-                      </td>
-                      <td className='p-4'>{user.fname}</td>
-                      <td className='p-4'>{user.lname}</td>
-                      <td className='p-4'>{user.nid}</td>
-                      <td className='p-4'>{user.email}</td>
-                      <td className='p-4'>{user.role}</td>
+                      <path
+                        fill-rule='evenodd'
+                        d='M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z'
+                        clip-rule='evenodd'
+                      ></path>
+                    </svg>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href='#'
+                    className='border-color-gray rounded-full border bg-white px-3 py-2 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                  >
+                    1
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href='#'
+                    className='border-color-gray rounded-full border bg-white px-3 py-2 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                  >
+                    2
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href='#'
+                    className='border-color-gray rounded-full border bg-white px-3 py-2 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                  >
+                    3
+                  </a>
+                </li>
 
-                      <td>
-                        <Link to={`#edit-${user.id}`} className='h-[100px] w-10 text-blue-600'>
-                          <Edit />
-                        </Link>{' '}
-                        <Link to={`#view-${user.id}`} className='text-green-600'>
-                          <Visibility />
-                        </Link>{' '}
-                        <Link to={`#delete-${user.id}`} className='text-red-600'>
-                          <Delete />
-                        </Link>
-                      </td>
-                    </tr>
-                  ),
-                )}
-              </tbody>
-            </table>
-          </div>
+                <li>
+                  <a
+                    href='#'
+                    className='block rounded-r-lg bg-white px-3 py-2 leading-tight  text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                  >
+                    <span className='sr-only'>Next</span>
+                    <svg
+                      className='h-5 w-5'
+                      aria-hidden='true'
+                      fill='currentColor'
+                      viewBox='0 0 20 20'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <path
+                        fill-rule='evenodd'
+                        d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z'
+                        clip-rule='evenodd'
+                      ></path>
+                    </svg>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </table>
         </div>
       </section>
     </Sidebar>
---
+++ src/screens/UpdateRoleScreen.tsx
@@ -0,0 +1,14 @@
+import { Edit } from "@material-ui/icons"
+import { Link } from "react-router-dom"
+
+
+export default function UpdateRoleScreen() {
+
+return (
+<div>
+    
+</div>
+
+);
+
+}
